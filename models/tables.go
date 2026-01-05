45d327457f612f28a5c3caf93cecf13a903da90a
vimaurya <mauryavikashhere@gmail.com>
2026-01-05 17:49:43 +0530

    Modified schema and and added lock claim in jwt

--- models/tables.go
+++ models/tables.go
@@ -8,7 +8,9 @@ type UserProfile struct {
 	PhoneNumber string `gorm:"column:phone_number"`
 	Password    string `gorm:"column:password"`
 	// Verified    bool   `gorm:"column:verified"`
-	Role string `gorm:"column:role"`
+	Role      string `gorm:"column:role"`
+	Lock      bool   `gorm:"column:lock"`
+	CreatedAt int64  `gorm:"column:created_at"`
 }
 
 func (UserProfile) TableName() string {
